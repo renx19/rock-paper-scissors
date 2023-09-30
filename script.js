1c45b1f6f22c9af2a4b422812f6b7f203af95b4d
renx19 <1markjustinborja1@gmail.com>
2023-09-30 19:17:50 +0800

    s

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
 var you;
 var yourScore = 0;
 var opponent;
 var opponentScore = 0;
-
 var choices = ["rock", "paper", "scissors"];
 
 window.onload = function() {
     for (let i = 0; i < 3; i++) {
-        // <img id="rock" src="rock.png">
         let choice = document.createElement("img");
         choice.id = choices[i];
         choice.src = choices[i] + ".png";
@@ -17,46 +14,63 @@ window.onload = function() {
     }
 }
 
+function announceResult(result) {
+    const synth = window.speechSynthesis || window.webkitSpeechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(result);
+    synth.speak(utterance);
+    
+}
+
 function selectChoice() {
     you = this.id;
     document.getElementById("your-choice").src = you + ".png";
 
-    //random for oppponent
-    opponent = choices[Math.floor(Math.random() * 3)]; //0- .999999 * 3 = 0-2.99999
+    opponent = choices[Math.floor(Math.random() * 3)];
     document.getElementById("opponent-choice").src = opponent + ".png";
 
-    //check for winner
     if (you == opponent) {
         yourScore += 1;
         opponentScore += 1;
-    }
-    else {
+        announceResult("It's a tie!");
+        updateResultText("It's a tie!");
+    } else {
         if (you == "rock") {
             if (opponent == "scissors") {
                 yourScore += 1;
-            }
-            else if (opponent == "paper") {
+                announceResult("You win!");
+                updateResultText("You win!");
+            } else if (opponent == "paper") {
                 opponentScore += 1;
+                announceResult("Opponent wins!");
+                updateResultText("Opponent wins!");
             }
-        }
-        else if (you == "scissors") {
+        } else if (you == "scissors") {
             if (opponent == "paper") {
                 yourScore += 1;
-            }
-            else if (opponent == "rock") {
+                announceResult("You win!");
+                updateResultText("You win!");
+            } else if (opponent == "rock") {
                 opponentScore += 1;
+                announceResult("Opponent wins!");
+                updateResultText("Opponent wins!");
             }
-        }
-        else if (you == "paper") {
+        } else if (you == "paper") {
             if (opponent == "rock") {
                 yourScore += 1;
-            }
-            else if (opponent == "scissors") {
+                announceResult("You win!");
+                updateResultText("You win!");
+            } else if (opponent == "scissors") {
                 opponentScore += 1;
+                announceResult("Opponent wins!");
+                updateResultText("Opponent wins!");
             }
         }
     }
 
     document.getElementById("your-score").innerText = yourScore;
     document.getElementById("opponent-score").innerText = opponentScore;
+}
+
+function updateResultText(result) {
+    document.getElementById("result-text").innerText = result;
 }
